decfaf1421749955a72947c471e76a42eb934a2b
chxp <33443763+chxpdotdev@users.noreply.github.com>
2025-10-18 16:55:44 -0700

    add x86_linux builder

--- modules/darwin/default.nix
+++ modules/darwin/default.nix
@@ -1,10 +1,18 @@
-# This is your nix-darwin configuration.
-# For home configuration, see /modules/home/*
-{
+{flake, ...}: {
   imports = [
+    # An existing Linux builder is needed to initially bootstrap `nix-rosetta-builder`.
+    # If one isn't already available: comment out the `nix-rosetta-builder` module below,
+    # uncomment this `linux-builder` module, and run `darwin-rebuild switch`:
+    # {nix.linux-builder.enable = true;}
+    # Then: uncomment `nix-rosetta-builder`, remove `linux-builder`, and `darwin-rebuild switch`
+    # a second time. Subsequently, `nix-rosetta-builder` can rebuild itself.
+    flake.inputs.nix-rosetta-builder.darwinModules.default
+
     ./common
     ./homebrew.nix
   ];
+
+  nix-rosetta-builder.onDemand = true;
 
   # Use TouchID for `sudo` authentication
   security.pam.services.sudo_local.touchIdAuth = true;
